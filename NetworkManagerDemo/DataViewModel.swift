14a6377ea904cc73aa5f49d40d15e85c3d47d8b4
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-03-13 15:17:33 +0100

    extract DataViewModel to his own file and make it generic

---
+++ NetworkManagerDemo/DataViewModel.swift
@@ -0,0 +1,57 @@
+//
+//  DataViewModel.swift
+//  NetworkManagerDemo
+//
+//  Created by Eric on 13/03/2026.
+//
+
+import SwiftUI
+
+@Observable
+class DataViewModel<T: Decodable> {
+    var data: [T] = []
+    private let manager = NetworkManager.shared
+    var networkError: NetworkError? = nil
+    var isLoading = false
+    let urlString: String
+
+    init(urlString: String) {
+        self.urlString = urlString
+    }
+
+    func fetchData() async {
+        isLoading = true
+        networkError = nil
+
+        defer { isLoading = false }
+
+        #if DEBUG
+        try? await Task.sleep(for: .seconds(2))
+        #endif
+        
+        do {
+            data = try await manager.fetchAndDecodeJSON(from: urlString)
+        } catch let error {
+            networkError = error
+        }
+    }
+}
+
+struct Loader: ViewModifier {
+    let isLoading: Bool
+    let title: String
+
+    func body(content: Content) -> some View {
+        if isLoading {
+            ProgressView("Loading \(title)")
+        } else {
+            content
+        }
+    }
+}
+
+extension View {
+    func withLoader(isLoading: Bool, title: String) -> some View {
+        modifier(Loader(isLoading: isLoading, title: title))
+    }
+}
