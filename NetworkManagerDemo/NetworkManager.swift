60647f383838f9f143e394dd85f57806a6d3e0dd
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-03-13 13:40:05 +0100

    Make generic T

---
+++ NetworkManagerDemo/NetworkManager.swift
@@ -0,0 +1,82 @@
+//
+//  NetworkManager.swift
+//  NetworkManagerDemo
+//
+//  Created by Eric on 12/03/2026.
+//
+
+import Foundation
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    private init() { }
+
+    func fetchAndDecodeJSON<T: Decodable>(from url: String) async -> T? {
+        guard let url = URL(string: url) else {
+            print("Invalid URL")
+            return nil
+        }
+        do {
+            let (data, response) = try await URLSession.shared.data(from: url)
+
+            guard let httpResponse = response as? HTTPURLResponse else {
+                print("Network error: Response was not HTTPURLResponse")
+                return nil
+            }
+
+            guard (200...299).contains(httpResponse.statusCode) else {
+                print("HTTP error: status code \(httpResponse.statusCode)")
+                return nil
+            }
+
+            do {
+                let decoder = JSONDecoder()
+                return try decoder.decode(T.self, from: data)
+            } catch let error as DecodingError {
+                print(decodingError(error: error))
+                return nil
+            } catch {
+                print("Decoding error: \(error.localizedDescription)")
+                print("Data as string: \(String(data: data, encoding: .utf8) ?? "Unable to convert data to string")")
+                return nil
+            }
+
+        } catch {
+            print("Request error \(error.localizedDescription)")
+            return nil
+        }
+    }
+
+    func decodingError(error: DecodingError) -> String {
+        switch error {
+        case .typeMismatch(let type, let context):
+            """
+            Decoding Error: Type mismatch for type \(type)
+            Context: \(context.debugDescription)
+            Coding path: \(context.codingPath.map { $0.stringValue }.joined(separator: " -> "))
+            """
+        case .valueNotFound(let type, let context):
+            """
+            Decoding Error: Value of type \(type) not found
+            Context: \(context.debugDescription)
+            Coding path: \(context.codingPath.map { $0.stringValue }.joined(separator: " -> "))
+            """
+        case .keyNotFound(let codingKey, let context):
+            """
+            Decoding Error: Key '\(codingKey.stringValue)' not found
+            Context: \(context.debugDescription)
+            Coding path: \(context.codingPath.map { $0.stringValue }.joined(separator: " -> "))
+            """
+        case .dataCorrupted(let context):
+            """
+            Decoding Error: Data corrupted
+            Context: \(context.debugDescription)
+            Coding path: \(context.codingPath.map { $0.stringValue }.joined(separator: " -> "))
+            """
+        @unknown default:
+            """
+            Unknown error: \(error.localizedDescription)
+            """
+        }
+    }
+}
